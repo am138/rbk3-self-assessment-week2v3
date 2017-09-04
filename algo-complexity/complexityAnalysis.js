861c1abd540b25cf0c584750d597a927bb573bd5
Hananmajali <hananmajali@gmail.com>
2017-09-04 09:44:02 -0400

    haha

--- algo-complexity/complexityAnalysis.js
+++ algo-complexity/complexityAnalysis.js
@@ -5,7 +5,16 @@
 
 */
 1- constant complexity O(1)
+  here no itration over the input regardless how many code line inside the funtion 
+
 2- linear complexity O(n)
+here there is iteration over input ex.array and make some functionality on each element
+
 3- dublicate/sequare complexity O(n*n)
+here we will find in the code nested itration over the input ex. two nested loop over array 
+to do some thing 
+
 4- O(log(n))
+here we will find itaration over the input , but here we will iterate over spisific parte in
+the input 
 
