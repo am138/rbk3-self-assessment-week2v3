791d650b103c3684307109c37693dcaaa5555426
abeer-shami <abeer.shami887@gmail.com>
2017-09-03 23:44:20 -0700

    hehe

--- algo-complexity/complexityAnalysis.js
+++ algo-complexity/complexityAnalysis.js
@@ -2,5 +2,10 @@
 // Be as clear and complete as necessary to demonstrate your mastery.
 // Write your explanation below as a comment
 /*
+algorithmic time complexity it is meaning the time that will the function take it to run as a function 
+and there is many kindes
+o(1)
+O(n)
+O(n^2)
 
 */
