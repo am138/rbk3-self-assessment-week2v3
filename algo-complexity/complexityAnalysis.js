bef06c88e7e0048c8baf82d556db6d610ed926a8
maisaa <maisaa.alkhedr@gmail.com>
2017-09-04 09:41:05 +0300

    hehe! 2

--- algo-complexity/complexityAnalysis.js
+++ algo-complexity/complexityAnalysis.js
@@ -2,5 +2,14 @@
 // Be as clear and complete as necessary to demonstrate your mastery.
 // Write your explanation below as a comment
 /*
+[constant] : that's mean the growth for this function is fixd
+
+[log(n)] : that's mean the growth for this function 
+like binary search each time i just check a half of the array.
+
+[n]  : that's mean the growth for this function is liner.
+
+[n^2] : that's mean the growth for this function is so fast by the time.
+
 
 */
