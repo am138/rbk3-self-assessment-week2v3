b440f583acf12a3f588633d1fda7e6b8672cd029
osamaths <osama.alshbikat@gmail.com>
2017-09-04 09:14:55 +0300

    complixityAnalysis completed

--- algo-complexity/complexityAnalysis.js
+++ algo-complexity/complexityAnalysis.js
@@ -2,5 +2,8 @@
 // Be as clear and complete as necessary to demonstrate your mastery.
 // Write your explanation below as a comment
 /*
-
+	1- Constant: we need only one instruction to complete the algorethim.
+	2- Logarithim: we check half of the array each loop, so no need to loop all elements.
+	3- Linear: we need n instructions to complete, we need loop to loop all elements to apply the instruction on.
+	4- Quadratic (O(n^2)): we need to repeat the repeated instruction squared time, nested loops.
 */
