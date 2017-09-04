4668dbc6273a88f6271946fdaccb9b377a6084a1
AlaaRBKgit config --global  user.email Alshyoukhi94@gmail.com <rbk16@RBK16s-Mac-mini.local>
2017-09-04 09:43:11 +0300

    haha

--- js-inheritance-pseudo-classical/js-inheritance-pseudo-classical.js
+++ js-inheritance-pseudo-classical/js-inheritance-pseudo-classical.js
@@ -39,3 +39,31 @@ var makeSmartPhone = function(phoneNumber, email) {
 };
 
 // your code is here
+var makePhone = function(phoneNumber) {
+ 
+
+  this.phoneNumber = phoneNumber;
+  makePhone.prototype.send = function(recipientPhoneNumber, message) {
+    return 'sending the message "' + message + '" to the phone number ' + recipientPhoneNumber + ' from ' + this.phoneNumber;
+  };
+
+  
+};
+
+var makeSmartPhone = function(phoneNumber, email) {
+  makePhone.call(this,phoneNumber);
+  this.oldSend = this.send;
+
+  this.email = email;
+  makeSmartPhone.prototype.send = function(recipientPhoneNumberOrEmail, message) {
+    if (typeof recipientPhoneNumberOrEmail === 'number') {
+      // We need `.call` here to make sure that `this` will reference our smart phone in makePhone's send
+      return oldSend.call(this, recipientPhoneNumberOrEmail, message);
+    } else {
+      return 'sending the message "' + message + '" to email ' + recipientPhoneNumberOrEmail + ' from ' + this.email;
+    }
+  };
+
+  
+};
+makeSmartPhone.constructor.makePhone=makeSmartPhone;
