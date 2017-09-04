d80ebc95ff7b1b7ec24b10e2334a980491443691
mazenalchalah <mazenalchalah@gmail.com>
2017-09-04 09:39:14 +0300

    hehe!

--- algo-complexity/complexityAnalysis.js
+++ algo-complexity/complexityAnalysis.js
@@ -2,5 +2,8 @@
 // Be as clear and complete as necessary to demonstrate your mastery.
 // Write your explanation below as a comment
 /*
-
+Bigo(1):constant time, input and out same size
+Bigo(n):linear time ,input size increase and cahnge
+Bigo(n^2):square time, input size cahnge by square
+Bigo(log(n)):logartim time, size cahnge by logaritem
 */
