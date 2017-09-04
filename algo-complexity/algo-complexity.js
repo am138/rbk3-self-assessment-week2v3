0a4606f7f627de98bc7e223179c6e39f26f0637a
osamaths <osama.alshbikat@gmail.com>
2017-09-04 09:06:45 +0300

    algo-complixity completed

--- algo-complexity/algo-complexity.js
+++ algo-complexity/algo-complexity.js
@@ -14,7 +14,7 @@
 var retrieve = function(key) {
   var hash = 0;
   for (var i = 0; i < key.length; i++) {
-    hash = (hash + Math.pow(i, hash)) % array.length;
+    hash = (hash + Math.pow(i, hash)) % array.length; //=====> for the key letters it's Linear, but becuase of the 'key never get very large' the key won't affect the function work, so the proccess to retrieve is 'Constant'.
   }
   return array[hash];
 };
@@ -32,7 +32,7 @@ var sortedArrayContainsItem = function(array, item) {
   if (array[center] === item) {
     return true;
   }
-  var halfOfArray = item < array[center] ? array.slice(0, center) : array.slice(center);
+  var halfOfArray = item < array[center] ? array.slice(0, center) : array.slice(center); //=====> O(log n), becuase each time we take HALF of the array.
   return sortedArrayContainsItem(halfOfArray, item);
 };
 
@@ -46,7 +46,7 @@ var sortedArrayContainsItem = function(array, item) {
 var hasDuplicates = function(array) {
   for (var i = 0; i < array.length; i++) {
     var item = array[i];
-    if (array.slice(i + 1).indexOf(item) !== -1) {
+    if (array.slice(i + 1).indexOf(item) !== -1) { //=======> time complixity here is O(n^2), becuase we have a loop to array length, and we have indexOf already has loop to search in an array.
       return true;
     }
   }
