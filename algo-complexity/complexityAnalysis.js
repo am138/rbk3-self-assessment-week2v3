fc5fb0f745343da00b0d0a3ed7fb8b768353361d
lenaSalamat <lenasalamat96@gmail.com>
2017-09-04 09:14:12 +0300

    firest1 commit

--- algo-complexity/complexityAnalysis.js
+++ algo-complexity/complexityAnalysis.js
@@ -4,3 +4,14 @@
 /*
 
 */
+// there is time complexity O(1) it is the better whay to do function and it is the smple whay to it comes
+// with the ordenare function that don't tack and need a for loop.
+
+
+//the other one is the O(n) it comes with the for loop and it is not the best whay to Write a function 
+
+// and there is the O(n*n) it is a function contanes a tow for loops and the tow of them after each other (seperatly)
+// and it is not a good whay to 
+
+
+//and there is O(2n) it has tow for loop and the tow are conected to each other insid each other
