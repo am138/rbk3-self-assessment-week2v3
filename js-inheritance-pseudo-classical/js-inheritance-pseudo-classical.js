6591922c8759b42468fd26daec2c54cd01ed04bb
kamellaba <kamelallababidi@gmail.com>
2017-09-04 09:11:27 +0300

    haha

--- js-inheritance-pseudo-classical/js-inheritance-pseudo-classical.js
+++ js-inheritance-pseudo-classical/js-inheritance-pseudo-classical.js
@@ -11,31 +11,40 @@
 // USE THE CONSTRUCTOR FUNCTIONS LOCATED AT THE END OF THIS FILE
 
 var makePhone = function(phoneNumber) {
-  var result = {};
-
-  result.phoneNumber = phoneNumber;
-  result.send = function(recipientPhoneNumber, message) {
-    return 'sending the message "' + message + '" to the phone number ' + recipientPhoneNumber + ' from ' + this.phoneNumber;
-  };
-
-  return result;
+  //var result = {};
+  this.phoneNumber = phoneNumber;
+ // return result;
 };
-
+  makePhone.prototype.send=function(recipientPhoneNumber, message){
+    return 'sending the message "' + message + '" to the phone number ' + 
+    recipientPhoneNumber + ' from ' + this.phoneNumber;
+  }
 var makeSmartPhone = function(phoneNumber, email) {
-  var phone = makePhone(phoneNumber);
-  var oldSend = phone.send;
+  //var phone = makePhone(phoneNumber);
+  this.oldSend = makePhone.send;
+  makePhone.call(this,phoneNumber);
+  this.email=email;
+  //phone.email = email;
+  // phone.send = function(recipientPhoneNumberOrEmail, message) {
+  //   if (typeof recipientPhoneNumberOrEmail === 'number') {
+  //     // We need `.call` here to make sure that `this` will reference our smart phone in makePhone's send
+  //     return oldSend.call(this, recipientPhoneNumberOrEmail, message);
+  //   } else {
+  //     return 'sending the message "' + message + '" to email ' + recipientPhoneNumberOrEmail + ' from ' + this.email;
+  //   }
+  // };
 
-  phone.email = email;
-  phone.send = function(recipientPhoneNumberOrEmail, message) {
-    if (typeof recipientPhoneNumberOrEmail === 'number') {
+//  return phone;
+};
+makeSmartPhone.prototype=Object.create(makePhone.prototype);
+makeSmartPhone.prototype.send=function(recipientPhoneNumberOrEmail, message){
+  if (typeof recipientPhoneNumberOrEmail === 'number') {
       // We need `.call` here to make sure that `this` will reference our smart phone in makePhone's send
-      return oldSend.call(this, recipientPhoneNumberOrEmail, message);
+      return makePhone.prototype.send.call(this,recipientPhoneNumberOrEmail, message);
     } else {
       return 'sending the message "' + message + '" to email ' + recipientPhoneNumberOrEmail + ' from ' + this.email;
     }
-  };
-
-  return phone;
-};
-
+}
+makePhone.prototype.CONSTRUCTOR=makePhone;
+makeSmartPhone.prototype.CONSTRUCTOR=makeSmartPhone;
 // your code is here
